d580705c85b54231be2e0e19ba63656d55141893
mayyadah <miss.mayadah1@gmail.com>
2021-07-08 19:41:34 +0300

    edit route

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ import Register from "views/examples/Register.js";
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/" exact render={props => <Index {...props} />} />
+      <Route path="/" exact render={props => <Profile {...props} />} />
       <Route
         path="/landing-page"
         exact
